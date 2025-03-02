e9663e6a9013838b02b5052fa713f105c68c9c2e
yashsh107 <yash.sharma21883@gmail.com>
2025-03-02 17:03:53 +0530

    function to remove element from specific index

--- linked_list/single_linked_list.py
+++ linked_list/single_linked_list.py
@@ -62,7 +62,32 @@ class SingleLinkedList :
             count +=1
             itr = itr.next
 
-        print(count)            
+        return count   
+
+    def insert_values(self, list) :
+        for data in list :
+            self.insert_at_end(data)
+
+    def remove_at(self, index) :
+        if index < 0 or index >= self.get_length() :
+            raise Exception("invalid index")
+        
+        itr = self.head     
+
+        if index == 0 :
+            self.head = itr.next
+            return
+        
+        count = 0
+
+        while itr :
+            if count == index - 1 :
+                itr.next = itr.next.next
+                break 
+            itr = itr.next 
+            count += 1
+    
+
 
 
 
@@ -70,9 +95,11 @@ class SingleLinkedList :
 if __name__ == '__main__' :
     ll = SingleLinkedList()
     
-    ll.insert_at_begining(10)
-    ll.insert_at_begining(20)
-    ll.insert_at_end(30)
+    # ll.insert_at_begining(10)
+    # ll.insert_at_begining(20)
+    # ll.insert_at_end(30)
+    # ll.get_length()
+    ll.insert_values(["banana", "apple", "mango", "seeds"])
+    ll.remove_at(2)
     ll.insert_at(2, "yash")
     ll.print()
-    ll.get_length()
